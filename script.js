8f5055a6d85d7a6f27951ba5b1c9f29805960894
Murillo José de Azevedo <murilloazevedo2@gmail.com>
2025-07-09 18:59:14 -0300

    Aula 02

---
+++ script.js
@@ -0,0 +1,12 @@
+const apiKeyInput = document.getElementById('apiKey');
+const gameSelect = document.getElementById('gameSelect');
+const questionInput = document.getElementById('questionInput');
+const askButton = document.getElementById('askButton');
+const form = document.getElementById('form');
+const aiResponse = document.getElementById('aiResponse');
+
+const enviarFormulario = (event) => {
+  event.preventDefault()
+}
+
+form.addEventListener('submit', enviarFormulario)
