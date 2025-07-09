874f052ee369a336a4f5db5335423029b944db04
Murillo José de Azevedo <murilloazevedo2@gmail.com>
2025-07-09 20:02:36 -0300

    Aula 03

--- script.js
+++ script.js
@@ -5,8 +5,34 @@ const askButton = document.getElementById('askButton');
 const form = document.getElementById('form');
 const aiResponse = document.getElementById('aiResponse');
 
-const enviarFormulario = (event) => {
-  event.preventDefault()
+const perguntarAI = async () => {
+  
 }
 
+const enviarFormulario = (event) => {
+  event.preventDefault()
+  const apiKey = apiKeyInput.value
+  const game = gameSelect.value
+  const question = questionInput.value
+
+  if (apiKey == '' || game == '' || question == '') {
+    alert('Por favor, preencha todos os campos')
+    return
+  }
+
+  askButton.disabled = true
+  askButton.textContent = 'Perguntando...'
+  askButton.classList.add('loading')
+
+  try {
+
+  } catch (error) {
+    console.log('Erro: ', error)
+  } finally {
+    askButton.disabled = false
+    askButton.textContent = "Perguntar"
+    askButton.classList.remove('loading')
+  }
+
+}
 form.addEventListener('submit', enviarFormulario)
