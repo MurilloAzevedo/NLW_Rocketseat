2283d17ca9a273b50379a879c3f61681e6d4cf45
Murillo José de Azevedo <murilloazevedo2@gmail.com>
2025-07-10 12:56:07 -0300

    Aula 03

--- script.js
+++ script.js
@@ -5,11 +5,38 @@ const askButton = document.getElementById('askButton');
 const form = document.getElementById('form');
 const aiResponse = document.getElementById('aiResponse');
 
-const perguntarAI = async () => {
-  
+//chave API: AIzaSyADxQhWEONUtD5XRLQFT51SdA7ooRyBZ0Q
+
+const perguntarAI = async (question, game, apiKey) => {
+  const model = "gemini-2.5-flash"
+  const geminiURL = `https://generativelanguage.googleapis.com/v1beta/models/${model}:generateContent?key=${apiKey}`
+  const pergunta = `
+    Tenho o jogo ${game}, e quero saber ${question}
+  `
+  const contents = [{
+    parts: [{
+      text: pergunta
+    }]
+  }]
+
+  //chamar API
+  const response = await fetch(geminiURL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      contents
+    })
+  })
+
+  const data = await response.json()
+ 
+  return data.candidates[0].contents.parts[0].text
+
 }
 
-const enviarFormulario = (event) => {
+const enviarFormulario = async (event) => {
   event.preventDefault()
   const apiKey = apiKeyInput.value
   const game = gameSelect.value
@@ -26,12 +53,18 @@ const enviarFormulario = (event) => {
 
   try {
 
+    await perguntarAI(question, game, apiKey)
+
   } catch (error) {
+
     console.log('Erro: ', error)
+
   } finally {
+
     askButton.disabled = false
     askButton.textContent = "Perguntar"
     askButton.classList.remove('loading')
+
   }
 
 }
